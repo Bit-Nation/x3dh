55f50ba803f8cc205d58e95985d768f68ff1c0ae
Florian Lenz <flenz81@googlemail.com>
2018-05-29 19:42:59 +0300

    [pre_key_bundle] added description to PreKeyBundle

--- pre_key_bundle.go
+++ pre_key_bundle.go
@@ -1,10 +1,20 @@
 package x3dh
 
+// the PreKeyBundle holds information
+// about the person you would like to
+// start chatting with. You need to implement
+// this interface your self.
 type PreKeyBundle interface {
 	IdentityKey() PublicKey
 	SignedPreKey() PublicKey
 	PreKeySignature() []byte
 	OneTimePreKey() *PublicKey
+	// this is actually the method in which
+	// you need to check if the signature
+	// of the PreKey is valid. YOU HAVE
+	// TO DO this your self. In case this
+	// method returns false the process of
+	// creating an shared secret will be aborted.
 	ValidSignature() bool
 }
 
