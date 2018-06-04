417987ac3f207efd09c8489c490cdc26991067fd
Florian Lenz <flenz81@googlemail.com>
2018-06-04 17:02:15 +0300

    [tests] fixed x3dh tests

--- x3dh_test.go
+++ x3dh_test.go
@@ -2,8 +2,10 @@ package x3dh
 
 import (
 	"crypto/sha256"
+	"crypto/rand"
 	"testing"
 
+	
 	require "github.com/stretchr/testify/require"
 )
 
@@ -11,7 +13,7 @@ const testProtocolName = "testing"
 
 func TestX3dh_CalculateSecretNoOneTimePreKey(t *testing.T) {
 
-	c := &Curve25519{}
+	c := NewCurve25519(rand.Reader)
 
 	// alice id key pair
 	aliceIdKeyPair, err := c.GenerateKeyPair()
@@ -24,10 +26,10 @@ func TestX3dh_CalculateSecretNoOneTimePreKey(t *testing.T) {
 	// bob signed pre key pair
 	bobSignedPreKey, err := c.GenerateKeyPair()
 
-	aliceX := New(c, sha256.New(), testProtocolName, aliceIdKeyPair)
+	aliceX := New(&c, sha256.New(), testProtocolName, aliceIdKeyPair)
 
 	// calculate secret with bob's pre key bundle
-	secretAlice, aliceEphemeralKey, err := aliceX.CalculateSecret(TestPreKeyBundle{
+	initializedProtocol, err := aliceX.CalculateSecret(TestPreKeyBundle{
 		validSignature:  true,
 		identityKey:     bobIdKeyPair.PublicKey,
 		signedPreKey:    bobSignedPreKey.PublicKey,
@@ -35,30 +37,30 @@ func TestX3dh_CalculateSecretNoOneTimePreKey(t *testing.T) {
 	})
 	require.Nil(t, err)
 
-	bobX := New(c, sha256.New(), testProtocolName, bobIdKeyPair)
+	bobX := New(&c, sha256.New(), testProtocolName, bobIdKeyPair)
 	secretBob, err := bobX.SecretFromRemote(ProtocolInitialisation{
 		RemoteIdKey:        aliceIdKeyPair.PublicKey,
-		RemoteEphemeralKey: aliceEphemeralKey,
+		RemoteEphemeralKey: initializedProtocol.EphemeralKey,
 		MySignedPreKey:     bobSignedPreKey.PrivateKey,
 	})
 	require.Nil(t, err)
 
-	require.Equal(t, secretAlice, secretBob)
+	require.Equal(t, initializedProtocol.SharedSecret, secretBob)
 
 }
 
 func TestX3dh_CalculateSecretInvalidSignature(t *testing.T) {
 
-	c := &Curve25519{}
+	c := NewCurve25519(rand.Reader)
 
 	// alice id key pair
 	aliceIdKeyPair, err := c.GenerateKeyPair()
 	require.Nil(t, err)
 
-	aliceX := New(c, sha256.New(), testProtocolName, aliceIdKeyPair)
+	aliceX := New(&c, sha256.New(), testProtocolName, aliceIdKeyPair)
 
 	// since we set the signature to invalid an error should be returned
-	_, _, err = aliceX.CalculateSecret(TestPreKeyBundle{
+	_, err = aliceX.CalculateSecret(TestPreKeyBundle{
 		validSignature: false,
 	})
 	require.EqualError(t, err, PreKeyBundleInvalidSignature.Error())
